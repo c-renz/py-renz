675d626078edc0d73e9222d6d4bfb9f32a23d0fe
c-renz <chlarenzterrones@gmail.com>
2025-05-18 20:09:50 +0800

    until insert array part

---
+++ arrays_strings.py
@@ -0,0 +1,13 @@
+a = [1, 2, 3]
+# print(a[1])
+# basically O(1)
+
+# a.append(5) 0(1)
+# print(a)
+
+# a.pop() //removing the very end if empty, can specify the index // O(1)
+# print(a)
+
+
+# a[2] = 5 // inserting/replacing values inside array - O(n)
+# print(a)
