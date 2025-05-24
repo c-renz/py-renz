8004b13b1fd03ca05080b7d6d00010ad711723e7
c-renz <chlarenzterrones@gmail.com>
2025-05-24 22:16:22 +0800

    done array and strings

--- arrays_strings.py
+++ arrays_strings.py
@@ -1,4 +1,5 @@
-a = [1, 2, 3]
+# Arrays
+# a = [1, 2, 3]
 # print(a[1])
 # basically O(1)
 
@@ -11,3 +12,27 @@ a = [1, 2, 3]
 
 # a[2] = 5 // inserting/replacing values inside array - O(n)
 # print(a)
+
+# a.insert(2, 4) // insert to the right/next index from the specified index O(n)
+
+# a[2] = 5 //modify the value in the index, if the index is not present it will error O(1)
+
+# print(a[0]) // access O(1)
+
+# print(len(a)) //prints length of string or array O(1)
+
+
+# print(7 in a) searching through the array O(n)
+# a.append("test")
+# print("test" in a)
+# print("yes", "test" in a)
+
+# print(a)
+
+
+# Strings
+a = "abc"
+# b = a + "5" // append at the end of the string // O(1)
+# print(b)
+
+# print("Is 7 in a? : ", "7" in a) searching through the string O(n)
