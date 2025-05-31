3337f0e0914d30d798e3342a69f7e06996a7068e
c-renz <chlarenzterrones@gmail.com>
2025-05-31 17:43:48 +0800

    added sets

---
+++ Hashes.py
@@ -0,0 +1,21 @@
+# SETS ====================================================================
+# s = set()
+
+# s.add(2)
+# s.add(4)
+# s.add(6)
+
+# print(s)
+
+# Lookup any values on the sets
+# print(7 in s)
+
+# s.remove(6 if 4 in s else 2) #// -> O(1)
+# print(s)
+
+# Set construction from string - O(S) - S is the length of the string
+# string = "aaaaaaabbbcccddddddddeeeeeee111111111111111111"
+# sett = set(string)  # The order here depends on the has values
+# print(sett)
+# for x in sett:
+#     print(x)
